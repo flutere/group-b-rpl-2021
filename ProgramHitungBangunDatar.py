6139881f6c8d943a3d36917e915425d94c711e96
flutere <muhammad.alifudin@mhs.unsoed.ac.id>
2021-09-28 22:41:02 +0700

    file utama program perhitungan bangun datar

---
+++ ProgramHitungBangunDatar.py
@@ -0,0 +1,26 @@
+#PROJECT KALKULATOR BANGUN DATAR
+#identifikasi :
+#p = panjang
+#l = lebar
+#s = panjang sisi
+#r = Jari-Jari lingkaran
+#L = Hasil Luas Bangun
+#K = Hasil Keliling Bangun
+#menu_bangun = pilihan bangun datar
+#menu_operasi = pilihan operasi bangun
+
+
+print('[KALKULATOR LUAS DAN KELILING BANGUN DATAR]')
+print('-------------------------------------------')
+print('Menu Bangun :')
+print('1. Persegi Panjang')
+print('2. Persegi')
+print('3. Lingkaaran') 
+print('4. Keluar')
+menu_bangun=int(input('Masukkan jenis bangun datar diinginkan : '))
+print('-------------------------------------------')
+print('Menu Perhitungan:')
+print('1. Luas')
+print('2. Keliling')
+print('3. Keluar')
+menu_operasi=int(input('Masukkan jenis perhitungan yang diinginkan :'))
